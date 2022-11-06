5e82b08dd47b9e3818257e0ff032537dd0c2fbd0
agnimita943 <agnimita12@gmail.com>
2022-11-06 19:05:17 +0530

    Added project files

---
+++ codes/js/questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the father of computer?",
+    answer: "Charles Babbage",
+    options: [
+      "James Gosling",
+      "Charles Babbage",
+      "Dennis Ritchie",
+      "Bjarne Stroustrup"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Identify the following heap structure by selecting the best answer:",
+    answer: "max-heap structure",
+    options: [
+      "partially ordered heap",
+      "min-heap structure",
+      "max-heap structure",
+      "priority heap"
+    ]
+  },
+    {
+    numb: 5,
+    question: "XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+
+    {
+     numb: 6,
+     question: "Which of the following is the most powerful type of computer?",
+     answer: "Supercomputer",
+     options: [
+       "Supercomputer",
+       "Microcomputer",
+       "Minicomputer",
+       "Computer"
+     ]
+   },
+
+{
+  numb: 7,
+  question: "A term associated with the comparison of processing speeds of different computer systems is",
+  answer: "MIPS",
+  options: [
+    "CPS",
+    "EFTS",
+    "MPG",
+    "MIPS"
+  ]
+},
+];
